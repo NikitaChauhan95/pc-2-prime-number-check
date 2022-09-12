4942f4e3b1a6f15f0b3a43e4e9411b17895eb5f1
Nikita Chauhan <nikitachauhan64@gmail.com>
2022-09-12 12:06:55 +0530

    reformatted the code

--- src/main/java/com/jap/prime/PrimeCheckerImpl.java
+++ src/main/java/com/jap/prime/PrimeCheckerImpl.java
@@ -3,7 +3,15 @@ package com.jap.prime;
 public class PrimeCheckerImpl {
     public boolean primeNumberCheck(int number) {
         // write the logic here to check is a number is prime or not
-        return false;
+       /* for(int index = 2; index <= number/2; index++) {
+            if (number % index == 0){
+                return false;
+            }
+        }
+
+        */
+
+        return true;
     }
 
 }
--- src/test/java/com/jap/prime/PrimeCheckerImplTest.java
+++ src/test/java/com/jap/prime/PrimeCheckerImplTest.java
@@ -8,33 +8,34 @@ import org.junit.Before;
 import org.junit.Test;
 
 
-public class PrimeCheckerImplTest
-{
+public class PrimeCheckerImplTest {
     PrimeCheckerImpl primeChecker = null;
+
     @Before
-   public void setUp(){
+    public void setUp() {
         primeChecker = new PrimeCheckerImpl();
-   }
-   @After
-   public void tearDown(){
-       primeChecker = null;
-   }
-    @Test
-    public void givenANumberShouldAnswerWithTrue()
-    {
-        assertTrue( primeChecker.primeNumberCheck(53));
-        assertTrue( primeChecker.primeNumberCheck(17));
-        assertTrue( primeChecker.primeNumberCheck(103));
-        assertTrue( primeChecker.primeNumberCheck(3));
-        assertTrue( primeChecker.primeNumberCheck(7));
     }
+
+    @After
+    public void tearDown() {
+        primeChecker = null;
+    }
+
     @Test
-    public void givenANumberShouldAnswerWithFalse()
-    {
-        assertFalse( primeChecker.primeNumberCheck(0));
-        assertFalse( primeChecker.primeNumberCheck(1));
-        assertFalse( primeChecker.primeNumberCheck(4));
-        assertFalse( primeChecker.primeNumberCheck(112));
-        assertFalse( primeChecker.primeNumberCheck(90));
+    public void givenANumberShouldAnswerWithTrue() {
+        assertTrue(primeChecker.primeNumberCheck(53));
+        assertTrue(primeChecker.primeNumberCheck(17));
+        assertTrue(primeChecker.primeNumberCheck(103));
+        assertTrue(primeChecker.primeNumberCheck(3));
+        assertTrue(primeChecker.primeNumberCheck(7));
+    }
+
+    @Test
+    public void givenANumberShouldAnswerWithFalse() {
+        assertFalse(primeChecker.primeNumberCheck(0));
+        assertFalse(primeChecker.primeNumberCheck(1));
+        assertFalse(primeChecker.primeNumberCheck(4));
+        assertFalse(primeChecker.primeNumberCheck(112));
+        assertFalse(primeChecker.primeNumberCheck(90));
     }
 }
