49b47abb7907ec0238240381ba568bbf6fee73a4
Nikita Chauhan <nikitachauhan64@gmail.com>
2022-09-12 12:38:01 +0530

    passed all the test cases

--- src/main/java/com/jap/prime/PrimeCheckerImpl.java
+++ src/main/java/com/jap/prime/PrimeCheckerImpl.java
@@ -3,15 +3,23 @@ package com.jap.prime;
 public class PrimeCheckerImpl {
     public boolean primeNumberCheck(int number) {
         // write the logic here to check is a number is prime or not
-        for(int index = 2; index <= number/2; index++) {
-            if (number % index == 0){
+       /* for (int index = 2; index <= number / 2; index++) {
+            if (number % index == 0) {
                 return false;
             }
         }
-
-
-
         return true;
     }
 
+        */
+        if (number <= 1) {
+            return false;
+        }
+        for (int index = 2; index <= number / 2; index++) {
+            if (number % index == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
