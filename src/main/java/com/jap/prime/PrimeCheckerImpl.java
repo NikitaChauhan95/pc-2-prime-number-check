23b3a7c3c7bb3470da8d045a513bb721d778cd32
Nikita Chauhan <nikitachauhan64@gmail.com>
2022-09-12 12:07:44 +0530

    written the logic for primeNumberChecker

--- src/main/java/com/jap/prime/PrimeCheckerImpl.java
+++ src/main/java/com/jap/prime/PrimeCheckerImpl.java
@@ -3,13 +3,13 @@ package com.jap.prime;
 public class PrimeCheckerImpl {
     public boolean primeNumberCheck(int number) {
         // write the logic here to check is a number is prime or not
-       /* for(int index = 2; index <= number/2; index++) {
+        for(int index = 2; index <= number/2; index++) {
             if (number % index == 0){
                 return false;
             }
         }
 
-        */
+
 
         return true;
     }
